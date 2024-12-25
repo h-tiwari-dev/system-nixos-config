f9bcc65dec8bbe1a1590941d172d5993b518d96f
Harsh Tiwari <harshtiwari@Harshs-MacBook-Air.local>
2024-12-25 18:23:38 +0530

    working on fingerprint

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -31,6 +31,9 @@
       #   InitialKeyRepeat = 14;
       #   KeyRepeat = 1;
       # };
+      PAM = {
+        enableSudoTouchIdAuth = true;  # Enable Touch ID for sudo authentication
+      };
       dock = {
         autohide = true;
         orientation = "bottom";
